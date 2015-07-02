3e18793cb588c03a3587be63a462521075b04e47
Chris Mather <chris@eventedmind.com>
2015-07-01 17:59:10 -0700

    using the dependency object

--- app/app.js
+++ app/app.js
@@ -1,32 +1,30 @@
 if (Meteor.isClient) {
   Name = {
     _value: 'Chris',
-
-    _comps: {},
-
+    _dep: new Tracker.Dependency,
     set: function (value) {
       if (value !== this._value) {
         this._value = value;
-
-        for (var id in this._comps)
-          this._comps[id].invalidate();
+        this._dep.changed();
       }
     },
 
     get: function () {
-      if (Tracker.active) {
-        var c = Tracker.currentComputation;
-        this._comps[c._id] = c;
-      }
-
+      this._dep.depend();
       return this._value;
     }
   };
 
   sayHello = function sayHello () {
     var name = Name.get();
-    console.log('Name: ' + name);
+    console.log('Hello: ' + name);
   };
 
-  computation = Tracker.autorun(sayHello);
+  anotherFunc = function anotherFunc () {
+    var name = Name.get();
+    console.log('Hello 2: ' + name);
+  };
+
+  Tracker.autorun(sayHello);
+  Tracker.autorun(anotherFunc);
 }
