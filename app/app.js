9ff453d2d2aaae6082b029bd34c6a37c8b9ef4b8
Chris Mather <chris@eventedmind.com>
2015-07-01 17:09:12 -0700

    custom reactive data source part one

--- app/app.js
+++ app/app.js
@@ -1,22 +1,32 @@
 if (Meteor.isClient) {
-  sayHello = function sayHello () {
-    console.log('hello world');
+  Name = {
+    _value: 'Chris',
 
-    Tracker.afterFlush(function () {
-      console.log('after flush');
-    });
+    _comps: {},
+
+    set: function (value) {
+      if (value !== this._value) {
+        this._value = value;
+
+        for (var id in this._comps)
+          this._comps[id].invalidate();
+      }
+    },
+
+    get: function () {
+      if (Tracker.active) {
+        var c = Tracker.currentComputation;
+        this._comps[c._id] = c;
+      }
+
+      return this._value;
+    }
+  };
+
+  sayHello = function sayHello () {
+    var name = Name.get();
+    console.log('Name: ' + name);
   };
 
   computation = Tracker.autorun(sayHello);
-
-  invalidate = function () {
-    computation.invalidate();
-    computation.invalidate();
-    computation.invalidate();
-    computation.invalidate();
-    computation.invalidate();
-    computation.invalidate();
-    Tracker.flush();
-    console.log('called invalidate()');
-  };
 }
